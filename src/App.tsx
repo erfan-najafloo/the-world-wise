5b21b2646fcca650071d24bac079192f53ec08b3
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-05 18:02:02 +0330

    feat: HomePage background added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className=" relative h-screen overflow-hidden ">
+      <HomePage />
+    </div>
+  );
+}
+export default App;
