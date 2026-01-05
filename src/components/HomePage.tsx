2697fe0c971fa5f0d9af0b274767d193707a17bb
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-05 21:47:53 +0330

    feat: pages folder created & HomePage component moved into that

--- src/components/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-function HomePage() {
-  return (
-    <div className="h-[calc(100vh-2rem)] bg-hero-overlay bg-cover bg-center m-4 "></div>
-  );
-}
-
-export default HomePage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,27 @@
+import AppNav from "../components/AppNav";
+
+function HomePage() {
+  return (
+    <div className="h-[calc(100vh-5rem)] bg-hero-overlay bg-cover bg-center m-9 py-9 px-20 ">
+      <AppNav />
+      <section className="flex flex-col h-[85%] items-center justify-center gap-10 text-center">
+        <h1 className="text-7xl leading-[1.3]">
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2
+          className="w-[90%] text-3xl text-[var(--color-light--1)]
+ mb-10"
+        >
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <a className="cta">start tracking now</a>
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
