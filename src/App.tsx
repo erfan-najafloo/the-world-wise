615871338b938beea66b4397cc5f21a81ef30408
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-06 13:47:14 +0330

    feat: AppLayout route added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
