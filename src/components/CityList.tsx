bcfe2ed9d2f13962288fe45731f6db17e89259d1
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-06 22:24:44 +0330

    feat: CityList & CountryList components added

---
+++ src/components/CityList.tsx
@@ -0,0 +1,11 @@
+function CityList() {
+  return (
+    <ul>
+      <li>City A</li>
+      <li>City B</li>
+      <li>City C</li>
+    </ul>
+  );
+}
+
+export default CityList;
---
+++ src/components/CountryList.tsx
@@ -0,0 +1,11 @@
+function CountryList() {
+  return (
+    <ul>
+      <li>Country A</li>
+      <li>Country B</li>
+      <li>Country C</li>
+    </ul>
+  );
+}
+
+export default CountryList;
