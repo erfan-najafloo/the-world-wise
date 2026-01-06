191b1532d2faf38d7353bc758ce64770fbe5f98f
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-06 22:37:05 +0330

    feat: map implemented in Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,26 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 function Map() {
-  return <div>map</div>;
+  return (
+    <div className="flex-1 h-[calc(100vh - 4.8rem)] bg-[var(--color-dark--2)] relative">
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="h-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
