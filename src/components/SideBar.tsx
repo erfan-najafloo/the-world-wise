46437c67bc278a88ee5d2eaa2582362794efbd31
Erfan Najafloo <erfan.najafloo@gmail.com>
2026-01-06 22:27:23 +0330

    feat: SideBar UI added

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,20 @@
+import { Outlet } from "react-router";
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+
 function SideBar() {
-  return <div>shideBar</div>;
+  return (
+    <div className="flex flex-col basis-4xl items-center h-[calc(100vh - 4.8rem)] bg-(--color-dark--1) pt-12 pr-20 pb-14 pl-20">
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <footer className="mt-auto">
+        <p className="text-xl text-(--color-light--1)">
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default SideBar;
